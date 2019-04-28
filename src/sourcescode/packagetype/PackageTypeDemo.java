ea0e652aec1eeffa9523f8e12ef2832c7db50d7d
wangjin <1772657505@qq.com>
2019-04-28 15:29:53 +0800

    1

--- src/sourcescode/packagetype/PackageTypeDemo.java
+++ src/sourcescode/packagetype/PackageTypeDemo.java
@@ -9,5 +9,13 @@ public class PackageTypeDemo {
         //only true return true
         Boolean a =new Boolean("aaaa");     //false
         System.out.println(a);
+        //only true return true
+        Boolean b = new Boolean("sss");
+        System.out.println(b.booleanValue());   //false
+        Boolean b1 = new Boolean(true);
+        System.out.println(b1.toString());      //true
+        System.out.println(Boolean.toString(false)); //false
+        System.out.println(Boolean.hashCode(true)); //true 1231 false 1237
+        System.out.println(Boolean.getBoolean("aaa")); //false
     }
 }
