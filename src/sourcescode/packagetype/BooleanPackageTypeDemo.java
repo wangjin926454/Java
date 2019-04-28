9a9214ecf4a943c07b5eb2fd55d2724e58640c0e
wangjin <1772657505@qq.com>
2019-04-28 15:47:12 +0800

    1

--- src/sourcescode/packagetype/BooleanPackageTypeDemo.java
+++ src/sourcescode/packagetype/BooleanPackageTypeDemo.java
@@ -3,7 +3,7 @@ package sourcescode.packagetype;
 /**
  * 包装类源码分析
  * */
-public class PackageTypeDemo {
+public class BooleanPackageTypeDemo {
     public static void main(String[] args){
         System.out.println(Boolean.TYPE);       //boolean
         //only true return true
---
+++ src/sourcescode/packagetype/IntegerPackageTypeDemo.java
@@ -0,0 +1,12 @@
+package sourcescode.packagetype;
+
+/**
+ * int package type source code 
+ * */
+public class IntegerPackageTypeDemo {
+    public static void main(String[] args){
+        System.out.println(Integer.MIN_VALUE);  //-2147483648
+        System.out.println(Integer.MAX_VALUE);  //2147483647
+        System.out.println(Integer.TYPE);       //int
+    }
+}
