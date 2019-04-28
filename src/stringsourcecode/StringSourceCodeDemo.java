02e6e4b8ccbaefa189f585da8d48a2e210711266
wangjin <1772657505@qq.com>
2019-04-28 13:19:54 +0800

    1
    1

--- src/stringsourcecode/StringSourceCodeDemo.java
+++ src/stringsourcecode/StringSourceCodeDemo.java
@@ -13,5 +13,23 @@ public class StringSourceCodeDemo {
         System.out.println(System.currentTimeMillis()-be);
         System.out.println(result);
 */
+        String a = "tsst";
+        //返回index的unicode码
+        System.out.println(a.codePointAt(2));
+        //返回index前一个字符的unicode码
+        System.out.println(a.codePointBefore(2));
+        //equals与equalsIgnoreCase区别:区分大小写与不区分大小写
+        String a1 = "wangjin";
+        String a2 = "WANGJIN";
+        System.out.println(a1.equals(a2));          //false
+        System.out.println(a1.equalsIgnoreCase(a2));//true
+        //equals与contentEquals:equals()后面必须为String类型
+        String s1="123";
+        String s2=new String("123");
+        StringBuilder sb=new StringBuilder("123");
+        System.out.println(s1.equals(s2));			//true
+        System.out.println(s1.contentEquals(s2));	//true
+        System.out.println(s1.equals(sb));			//false
+        System.out.println(s1.contentEquals(sb));	//true
     }
 }
