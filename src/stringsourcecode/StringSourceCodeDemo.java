4429c5d4b78b6032c1b2d7647f32bf18b66f270c
wangjin <1772657505@qq.com>
2019-04-28 10:35:01 +0800

    1
    1

---
+++ src/stringsourcecode/StringSourceCodeDemo.java
@@ -0,0 +1,8 @@
+package stringsourcecode;
+
+public class StringSourceCodeDemo {
+    private static String  a = "wangjin";
+    public static void main(String[] args){
+        System.out.println(a.length());
+    }
+}
