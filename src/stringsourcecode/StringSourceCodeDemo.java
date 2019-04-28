ab046e194197db7ad1b32e47b785cc65b56006a7
wangjin <1772657505@qq.com>
2019-04-28 11:08:49 +0800

    1
    1

--- src/stringsourcecode/StringSourceCodeDemo.java
+++ src/stringsourcecode/StringSourceCodeDemo.java
@@ -1,8 +1,17 @@
 package stringsourcecode;
 
+import java.util.Arrays;
+
 public class StringSourceCodeDemo {
-    private static String  a = "wangjin";
+    //数组拷贝用System.arraycopy而不用Arrays.copyOf();
     public static void main(String[] args){
-        System.out.println(a.length());
+        /*long be = System.currentTimeMillis();
+        String  a = "wangjin";
+        char[] aa = a.toCharArray();
+        char[] result =new char[aa.length];
+        System.arraycopy(aa,0,result,0,a.length());
+        System.out.println(System.currentTimeMillis()-be);
+        System.out.println(result);
+*/
     }
 }
