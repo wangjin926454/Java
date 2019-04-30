6424d71064b79528d7d309a2f4578ee7c21a7f2b
wangjin <1772657505@qq.com>
2019-04-30 11:05:14 +0800

    1

---
+++ src/KeywordDemo.java
@@ -0,0 +1,10 @@
+/**
+ * java keywords 
+ * */
+public class KeywordDemo {
+    public static void main(String[] args){
+        System.out.println("this is a keyword: volatile.representative this object doesn't sorted in JVM");
+        System.out.println("this is a keyword: native.representative this method is native");
+        System.out.println("this is a keyword: transient.representative this method doesn't Serializable");
+    }
+}
---
+++ src/sourcescode/throwablesourcecode/ThrowableDemo.java
@@ -0,0 +1,13 @@
+package sourcescode.throwablesourcecode;
+
+/**
+ * Throwable source code demo
+ * */
+public class ThrowableDemo {
+    public Integer test(){
+        return 5;
+    }
+
+    public static void main(String[] args){
+    }
+}
