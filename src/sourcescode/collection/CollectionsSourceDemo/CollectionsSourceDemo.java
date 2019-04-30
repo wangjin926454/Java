8ea123cdc07c0655ba16b72aa7cc5c55b1acc633
wangjin <1772657505@qq.com>
2019-04-30 16:11:43 +0800

    wangjin

---
+++ src/sourcescode/collection/CollectionsSourceDemo/CollectionsSourceDemo.java
@@ -0,0 +1,12 @@
+package sourcescode.collection.CollectionsSourceDemo;
+
+import java.util.Collections;
+
+/**
+ * Collections source code demo
+ * */
+public class CollectionsSourceDemo {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ src/sourcescode/collection/mapandset/SortMapAndSortSet.java
@@ -0,0 +1,10 @@
+package sourcescode.collection.mapandset;
+
+
+/**
+ * SortMap And SortSet source code demo
+ * */
+public class SortMapAndSortSet {
+    public static void main(String[] args){
+    }
+}
