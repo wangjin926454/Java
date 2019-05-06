44adfa000011dafecda9a14c991add8a7bba2eaa
wangjin <1772657505@qq.com>
2019-05-06 15:03:27 +0800

    1

---
+++ src/sourcescode/systempackagevoidsourcecode/SystemPackageVoidDemo.java
@@ -0,0 +1,18 @@
+package sourcescode.systempackagevoidsourcecode;
+
+/**
+ * System Package Void Source Code demo
+ */
+
+public class SystemPackageVoidDemo {
+    public static void main(String[] args){
+        //implements java.lang.reflect.AnnotatedElement
+        //Package
+        //Void
+        //native method
+        System.gc();
+        System.out.println(System.console());
+        System.exit(0);
+    }
+
+}
