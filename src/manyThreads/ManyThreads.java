33da180efc3accd00ec4fbaf8eff31f360322155
wangjin926454 <1772657505@qq.com>
2019-09-02 21:46:49 +0800

    1

---
+++ src/manyThreads/ManyThreads.java
@@ -0,0 +1,13 @@
+package manyThreads;
+
+public class ManyThreads {
+    public static void main(String[] args){
+        Thread t = new Thread(){
+            public void run(){
+                System.out.println("test");
+            }
+        };
+        t.start();
+        t.getState();
+    }
+}
---
+++ src/manyThreads/PackageInfo.java
@@ -0,0 +1,7 @@
+package manyThreads;
+
+/**
+ * 多线程编程核心篇
+ * */
+public class PackageInfo {
+}
