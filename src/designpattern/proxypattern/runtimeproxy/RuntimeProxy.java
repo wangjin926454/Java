a230f7237d50621209f644b7677d4e3a5720e0c3
wangjin926454 <1772657505@qq.com>
2019-10-23 22:32:42 +0800

    Update RuntimeProxy.java

--- src/designpattern/proxypattern/runtimeproxy/RuntimeProxy.java
+++ src/designpattern/proxypattern/runtimeproxy/RuntimeProxy.java
@@ -1,5 +1,7 @@
 package designpattern.proxypattern.runtimeproxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class RuntimeProxy {
@@ -7,15 +9,19 @@ public class RuntimeProxy {
     public static Object getProxyInstance(Object obj){
 
         return Proxy.newProxyInstance(obj.getClass().getClassLoader(),
-                obj.getClass().getInterfaces(), (proxy, method, args) -> {
+                obj.getClass().getInterfaces(), new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                         String methodName = method.getName();
                         System.out.println("method begin "+methodName+"   ");
                         Object result = method.invoke(obj,args);
                         System.out.println("method end "+methodName+"    ");
-                    return result;
+                        return result;
+                    }
                 });
     }
 }
+
 
 
 
